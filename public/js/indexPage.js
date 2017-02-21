242d6f36f624c9bd0e99ea8ec5fd1cb52a8fdb8a
ssvictorlin <ssvictorlin@gmail.com>
2017-02-20 21:02:54 -0800

    add calendar scroll

--- public/js/indexPage.js
+++ public/js/indexPage.js
@@ -5,13 +5,28 @@ $(document).ready(function() {
     $("#datepicker").datepicker({
             changeMonth: true,
             changeYear: true,
+            onSelect: function() { 
+            var dateObject = $(this).datepicker('getDate'); 
+            console.log('Month: '+ dateObject.getMonth()+ ', Date: '+ dateObject.getDate()+ ', Year: '+ dateObject.getFullYear());
+            var target = 3;
+            $('html, body').animate({
+                scrollTop: $('#'+target).offset().top
+            }, 500);
+        }
     })
-    .hide()
-    .click(function() {
+    .hide();
+    /*.click(function() {
       $(this).hide();
-    });
+    });*/
 
     $("#calendar-button").click(function() {
        $("#datepicker").toggle(); 
+       var calendarHeight = $('#datepicker').height();
+       if ($('#datepicker').is(':visible')) {
+            $('body').css('padding-top', calendarHeight+72+'px');
+        }
+        else {
+            $('body').css('padding-top', 72+'px');
+        }
     });
 });
