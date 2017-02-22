70fa93b71bef55b5e0177193122585249ecd6ae5
ssvictorlin <ssvictorlin@gmail.com>
2017-02-22 10:24:17 -0800

    scroll to selected date

--- public/js/indexPage.js
+++ public/js/indexPage.js
@@ -8,10 +8,17 @@ $(document).ready(function() {
             onSelect: function() { 
             var dateObject = $(this).datepicker('getDate'); 
             console.log('Month: '+ dateObject.getMonth()+ ', Date: '+ dateObject.getDate()+ ', Year: '+ dateObject.getFullYear());
-            var target = 3;
-            $('html, body').animate({
-                scrollTop: $('#'+target).offset().top
-            }, 500);
+
+            var dateText = weekday(dateObject.getDay()) + ' ' + monthToString(dateObject.getMonth()) + ' ' + 
+                        dateObject.getDate() + ', ' + dateObject.getFullYear();
+            console.log(dateText);
+            var ele = $("p:contains("+ dateText +")");
+            //console.log('divID found: ' + ele.offset().top);
+            if (ele.length) {
+                $('html, body').animate({
+                    scrollTop: ele.offset().top - 282
+                }, 500);
+            }
         }
     })
     .hide();
@@ -30,3 +37,77 @@ $(document).ready(function() {
         }
     });
 });
+
+
+function weekday(day) {
+    //console.log(day.getDay());
+    var d;
+    switch(day) {
+        case 1:
+            d = "Mon.";
+            break;
+        case 2:
+            d = "Tue.";
+            break;
+        case 3:
+            d = "Wed.";
+            break;
+        case 4:
+            d = "Thu.";
+            break;
+        case 5:
+            d = "Fri.";
+            break;
+        case 6:
+            d = "Sat.";
+            break;
+        case 0:
+            d = "Sun.";
+            break;
+    }
+    return d;
+}
+
+
+function monthToString(m) {
+    var monthText;
+    switch(m) {
+        case 0:
+            monthText = "Jan";
+            break;
+        case 1:
+            monthText = "Feb";
+            break;
+        case 2:
+            monthText = "Mar";
+            break;
+        case 3:
+            monthText = "Apr";
+            break;
+        case 4:
+            monthText = "May";
+            break;
+        case 5:
+            monthText = "Jun";
+            break;
+        case 6:
+            monthText = "Jul";
+            break;
+        case 7:
+            monthText = "Aug";
+            break;
+        case 8:
+            monthText = "Sep";
+            break;
+        case 9:
+            monthText = "Oct";
+            break;
+        case 10:
+            monthText = "Nov";
+            break;
+        case 11:
+            monthText = "Dec";
+            break;
+    }
+    return monthText;
+}
